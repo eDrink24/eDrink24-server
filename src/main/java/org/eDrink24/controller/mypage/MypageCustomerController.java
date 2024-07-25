59c9178b4e55f0ef21e5b82417c613263ca1306d
Young5097 <bigfaith0301@naver.com>
2024-07-25 10:52:28 +0900

    fix : MypageCustomerController.java 추가
    #4에서 push되지 않아서 별도 추가

---
+++ src/main/java/org/eDrink24/controller/mypage/MypageCustomerController.java
@@ -0,0 +1,22 @@
+package org.eDrink24.controller.mypage;
+
+import org.eDrink24.dto.customer.CustomerDTO;
+import org.eDrink24.service.customer.CustomerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MypageCustomerController {
+
+    CustomerService customerService;
+    public MypageCustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping(value = {"/selectCustomerMyPage/{loginId}"})
+    public CustomerDTO selectCustomerMyPage(@PathVariable String loginId) {
+        CustomerDTO customerDTO = customerService.selectCustomerMyPage(loginId);
+        return customerDTO;
+    }
+}

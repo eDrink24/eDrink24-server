79dcd0196966225d6909031f70a34c06f1fcbe18
Young5097 <bigfaith0301@naver.com>
2024-08-07 17:44:16 +0900

    [feature] pw 찾기 및 변경기능 구현
    - loginId, email을 통해 회원여부 확인
    - 확인됐으면 비밀번호 변경
    - 추후, 비밀번호 자리수 제한필요

--- src/main/java/org/eDrink24/service/customer/CustomerService.java
+++ src/main/java/org/eDrink24/service/customer/CustomerService.java
@@ -7,8 +7,12 @@ public interface CustomerService {
     public CustomerDTO saveCustomer(CustomerDTO customer);
     public CustomerDTO selectCustomerByLoginId(String loginId);
     public int updateCustomerToMyPage(CustomerDTO customerDTO);
+
     // 회원가입할때 아이디 중복체크
     public CustomerDTO customerIdCheck(String loginId);
+
     // 아이디 찾기
     public String findByUserNameAndEmail(String userName, String email);
+    // 비밀번호 찾기
+    public CustomerDTO findByLoginIdAndEmail(String loginId, String email);
 }

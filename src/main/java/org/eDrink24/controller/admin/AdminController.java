e3742dd6c1890d912d3d311b878de3bbaf4aa760
giuk-kim2 <gimgiug87@gmail.com>
2024-08-16 16:29:10 +0900

    [feat] 관리자페이지 즉시픽업 기능
    - 주문완료된 목록들을 즉시픽업 목록페이지에 보여주고 픽업완료 버튼을 누르면 즉시픽업 완료내역페이지에서 픽업완료된 것들의 내역을 보여줌.
    
    - 픽업완료된 목록들은 즉시픽업페이지에서 삭제되고 즉시픽업 완료내역페이지에서 보여줌.
    
    - 픽업완료 버튼 클릭 시 order테이블의 isCompleted는 true로 변경되고 orderhistory테이블의 changeStatus는 "PICKUPED", changeDate는 현재 시간으로 변경, 마지막으로 inventory의 quantity도 줄어듬.

---
+++ src/main/java/org/eDrink24/controller/admin/AdminController.java
@@ -0,0 +1,34 @@
+package org.eDrink24.controller.admin;
+
+import org.eDrink24.dto.admin.AdminDTO;
+import org.eDrink24.service.admin.AdminService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AdminController {
+
+    AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+    
+    @PutMapping(value = {"/updateStateAfterCompletedPickup/{ordersId}"})
+    public ResponseEntity<Void> updateStateAfterCompletedPickup(@PathVariable Integer ordersId) {
+        adminService.updateStateAfterCompletedPickup(ordersId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = {"/showPickupCompletedPage"})
+    public List<AdminDTO> showPickupCompletedPage() {
+        return adminService.showPickupCompletedPage();
+    }
+
+    @GetMapping(value = {"/showPickupPage"})
+    public List<AdminDTO> showPickupPage() {
+        return adminService.showPickupPage();
+    }
+}

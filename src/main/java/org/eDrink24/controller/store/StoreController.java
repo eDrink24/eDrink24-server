41c8ee98a77f43099ec05ea54b92d761a4103c62
Young5097 <bigfaith0301@naver.com>
2024-08-17 23:06:30 +0900

    [feature] findStoreByStoreId
    - storeId로 store entity 가져오는 api
    - /api/findStore/{storeId}

--- src/main/java/org/eDrink24/controller/store/StoreController.java
+++ src/main/java/org/eDrink24/controller/store/StoreController.java
@@ -1,5 +1,6 @@
 package org.eDrink24.controller.store;
 
+import lombok.extern.slf4j.Slf4j;
 import org.eDrink24.dto.store.StoreDTO;
 import org.eDrink24.service.store.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,23 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class StoreController {
     @Autowired
     StoreService storeService;
 
-    @GetMapping("/allStores")
+    @GetMapping("/api/findStore/{storeId}")
+    public ResponseEntity<StoreDTO> findStore(@PathVariable("storeId") int storeId) {
+        try {
+            StoreDTO storeDTO = storeService.findByStoreId(storeId);
+            log.info(storeDTO.toString());
+            return ResponseEntity.ok(storeDTO);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/api/allStores")
     public ResponseEntity<List<StoreDTO>> showAllStore() {
         List<StoreDTO> stores = storeService.findAllStores();
         return ResponseEntity.ok(stores);
--- src/main/java/org/eDrink24/repository/store/StoreRepository.java
+++ src/main/java/org/eDrink24/repository/store/StoreRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Integer> {
-
+    public Store findByStoreId(int storeId);
 }

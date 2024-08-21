98bd9c67e1e3bb05bc333fa35afa5624b726defd
talCSHN <djsxp7@naver.com>
2024-08-21 15:30:51 +0900

    [feat] 주문내역 조회
    showOrderHistory -> orderDate별 주문내역 조회

---
+++ src/main/java/org/eDrink24/service/order/OrderHistoryService.java
@@ -0,0 +1,16 @@
+package org.eDrink24.service.order;
+
+import org.eDrink24.dto.basket.BasketDTO;
+import org.eDrink24.dto.basket.BasketItemDTO;
+import org.eDrink24.dto.order.OrderTransactionDTO;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
+public interface OrderHistoryService {
+
+    // 주문내역 조회
+    public List<OrderTransactionDTO> showOrderHistory(Integer userId);
+
+}

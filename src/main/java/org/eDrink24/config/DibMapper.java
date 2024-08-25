602b48a734fc0b4a4deb62e181e3e62458cf852c
talCSHN <djsxp7@naver.com>
2024-08-25 20:28:00 +0900

    [feat] 상품 찜하기
    각 dibs 폴더에 찜하기 기능 추가

--- src/main/java/org/eDrink24/config/DibMapper.java
+++ src/main/java/org/eDrink24/config/DibMapper.java
@@ -2,7 +2,9 @@ package org.eDrink24.config;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.eDrink24.dto.dibs.DibDTO;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -12,7 +14,10 @@ public interface DibMapper {
     // 상품 찜하기(dibs 테이블에 저장)
     public void addDibs(DibDTO dibDTO);
 
+    // 찜 목록 삭제
+    public void cancelDIb(Integer userId, Integer productId);
+
     // 찜 목록 조회
-    public List<DibDTO> showAllDibs(String loginId);
+    public List<DibDTO> showAllDibs(Integer userId);
 
 }

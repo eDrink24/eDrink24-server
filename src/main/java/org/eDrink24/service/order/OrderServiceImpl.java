75731f7efa7dfc214434f1d80d3d91d2465da78b
Young5097 <bigfaith0301@naver.com>
2024-09-30 20:21:53 +0900

    [fix] 주문오류 해결
    - RESERVATION 주문에서 오류발생
    - Mapper 및 조건오류 수정

--- src/main/java/org/eDrink24/service/order/OrderServiceImpl.java
+++ src/main/java/org/eDrink24/service/order/OrderServiceImpl.java
@@ -61,7 +61,7 @@ public class OrderServiceImpl implements OrderService {
             for (OrderTransactionDTO orderTransaction : orderTransactionDTO) {
                 // 재고감소는 오늘픽업일때만, 예약픽업은 점주의 발주처리가 완료되어야 재고가 생김
                 String pickupType = orderTransaction.getPickupType();
-                if (!pickupType.equals("TODAY")) {
+                if (pickupType.equals("TODAY")) {
                     int storeId = orderTransaction.getStoreId();
                     int productId = orderTransaction.getProductId();
                     int quantity = orderTransaction.getOrderQuantity();
@@ -72,7 +72,9 @@ public class OrderServiceImpl implements OrderService {
                     map.put("quantity", quantity);
 
                     InventoryDTO inventory = inventoryMapper.findInventoryForUpdate(map);
+                    System.out.println(inventory);
                     if(inventory==null || inventory.getQuantity() < quantity) {
+                        System.out.println("!error!");
                         throw new IllegalArgumentException("재고 부족으로 주문불가"); // 나중에 전역예외처리 필요. 발생하면 아래 코드 실행X
                     }
                     inventoryMapper.updateInventory(map);
